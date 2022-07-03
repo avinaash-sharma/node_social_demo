fbdc595d75cab8f916add0aa9ab9a7d42a9ee24c
Avinash Sharma <avi.rohit13@hotmail.com>
2022-07-03 08:33:38 +0530

    heroku-deploy

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const PORT = 3000 || process.env.PORT;
+const format = require("date-format");
+console.log("Starting...");
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello, People!");
+});
+
+app.get("/api/v1/instagram", (req, res) => {
+  const instagramData = {
+    followers: 20,
+    following: 900,
+    date: format.asString("dd-MM-yyyy hh:mm:ss", new Date()),
+  };
+  res.status(200).json(instagramData);
+});
+
+app.get("/api/v1/facebook", (req, res) => {
+  const facebookData = {
+    followers: 20,
+    following: 900,
+    date: format.asString("dd-MM-yyyy hh:mm:ss", new Date()),
+  };
+  res.status(200).json({ facebookData });
+});
+
+app.get("/api/v1/linkedin", (req, res) => {
+  const linkedinData = {
+    followers: 20,
+    following: 900,
+    date: format.asString("dd-MM-yyyy hh:mm:ss", new Date()),
+  };
+  res.status(200).json(linkedinData);
+});
+
+app.get("/api/v1/:name", (req, res) => {
+    const params = req.params.name;
+    res.status(400).json(params);
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
